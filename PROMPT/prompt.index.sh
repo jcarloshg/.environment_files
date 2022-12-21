8fc0592926db77a2c08ec649649a044e3bc5d464
jcarloshg <carlosj12336@gmail.com>
2022-12-20 18:59:29 -0600

    ADD prompt

---
+++ PROMPT/prompt.index.sh
@@ -0,0 +1,14 @@
+setopt PROMPT_SUBST
+
+function _exit_code() {
+    local EXIT="$?"
+
+    if [ $EXIT -eq 0 ]; then
+        echo -n green
+    else
+        echo -n red
+    fi
+}
+
+# PROMPT='$(_exit_code)%n @ %d: '
+PROMPT='%{%F{$(_exit_code)}%}%n%{%f%S}% @ %d: '
