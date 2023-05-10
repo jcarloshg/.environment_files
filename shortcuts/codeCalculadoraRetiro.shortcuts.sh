b0acc244cf3f24abf33c80cec58721b89f2231de
jcarloshg654 <carlos@kokonutstudio.com>
2023-05-10 06:38:59 -0600

    upload codeCalculadra

--- shortcuts/codeCalculadoraRetiro.shortcuts.sh
+++ shortcuts/codeCalculadoraRetiro.shortcuts.sh
@@ -1,7 +1,6 @@
 function codeCalculadoraRetiro {
 
-    # /home/jose/Documentos/kokonut/calculadora_retiro/Coppel_Calculadora_Web/Coppel_Calculadora_web_Front
-    route="$HOME/Documentos/kokonut/calculadora_retiro/Coppel_Calculadora_Web/Coppel_Calculadora_web_Front"
+    route="$HOME/Documentos/kokonut/Coppel_Calculadora_Web/Coppel_Calculadora_web_Front"
 
     echo "${JUMPLINE} > executing...    ${MAGENTA}[     ${RED}cd ${MAGENTA}${route}     ]"
     NCOLOR
